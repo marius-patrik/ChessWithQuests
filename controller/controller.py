10f4fc27b89acb77ef1481d541c57f4a4810eee8
marius-patrik <marius-patrik@users.noreply.github.com>
2026-09-04 14:47:43 +0200

    Implement GameController class and unit tests

---
+++ controller/controller.py
@@ -0,0 +1,68 @@
+from typing import Optional, Tuple, List, Dict, Any
+
+try:
+    from model.game.manager import GameManager
+    from model.game.move import Move
+except ImportError:
+    from ..model.game.manager import GameManager
+    from ..model.game.move import Move
+
+
+class GameController:
+    def __init__(self, game_manager: Optional[GameManager] = None):
+        self.game_manager: GameManager = game_manager or GameManager()
+        self.selected_square: Optional[Tuple[int, int]] = None
+        self.highlighted_moves: List[Tuple[int, int]] = []
+
+    def select_square(self, pos: Tuple[int, int]) -> List[Tuple[int, int]]:
+        piece = self.game_manager.plocha.get_piece_at(pos)
+        if piece is not None and piece.getColor() == self.game_manager.aktivni_hrac:
+            self.selected_square = pos
+            self.highlighted_moves = self.game_manager.revizor_tahu.get_valid_moves(pos, self.game_manager.plocha)
+            return list(self.highlighted_moves)
+        self.selected_square = None
+        self.highlighted_moves = []
+        return []
+
+    def handle_square_click(self, pos: Tuple[int, int]) -> Dict[str, Any]:
+        if self.selected_square is None:
+            moves = self.select_square(pos)
+            return {
+                "action": "selected" if self.selected_square else "none",
+                "selected": self.selected_square,
+                "valid_moves": moves,
+            }
+        else:
+            piece = self.game_manager.plocha.get_piece_at(pos)
+            if piece is not None and piece.getColor() == self.game_manager.aktivni_hrac:
+                moves = self.select_square(pos)
+                return {
+                    "action": "reselected",
+                    "selected": self.selected_square,
+                    "valid_moves": moves,
+                }
+
+            move = Move(self.selected_square, pos)
+            success = self.game_manager.make_move(move)
+            prev_selected = self.selected_square
+            self.selected_square = None
+            self.highlighted_moves = []
+
+            return {
+                "action": "moved" if success else "invalid",
+                "success": success,
+                "from": prev_selected,
+                "to": pos,
+                "game_state": self.game_manager.get_stav(),
+            }
+
+    def reset_selection(self) -> None:
+        self.selected_square = None
+        self.highlighted_moves = []
+
+    def new_game(self) -> None:
+        self.game_manager = GameManager()
+        self.reset_selection()
+
+
+Controller = GameController
---
+++ tests/test_controller.py
@@ -0,0 +1,51 @@
+import pytest
+from controller.controller import GameController, Controller
+from model.game.manager import GameManager
+
+
+def test_controller_initialization():
+    ctrl = GameController()
+    assert ctrl.selected_square is None
+    assert ctrl.highlighted_moves == []
+    assert isinstance(ctrl.game_manager, GameManager)
+
+
+def test_controller_select_and_move_flow():
+    ctrl = GameController()
+    # Click 1: Click on White Pawn at (1, 4)
+    res1 = ctrl.handle_square_click((1, 4))
+    assert res1["action"] == "selected"
+    assert res1["selected"] == (1, 4)
+    assert (2, 4) in res1["valid_moves"]
+    assert (3, 4) in res1["valid_moves"]
+
+    # Click 2: Click on destination (3, 4)
+    res2 = ctrl.handle_square_click((3, 4))
+    assert res2["action"] == "moved"
+    assert res2["success"] is True
+    assert ctrl.selected_square is None
+    # Next turn is Black's turn (-1)
+    assert ctrl.game_manager.aktivni_hrac == -1
+
+
+def test_controller_reselect():
+    ctrl = GameController()
+    # Select e2 pawn
+    ctrl.handle_square_click((1, 4))
+    assert ctrl.selected_square == (1, 4)
+
+    # Click another white piece (d2 pawn)
+    res = ctrl.handle_square_click((1, 3))
+    assert res["action"] == "reselected"
+    assert ctrl.selected_square == (1, 3)
+
+
+def test_controller_new_game():
+    ctrl = GameController()
+    ctrl.handle_square_click((1, 4))
+    ctrl.handle_square_click((3, 4))
+    assert ctrl.game_manager.aktivni_hrac == -1
+
+    ctrl.new_game()
+    assert ctrl.game_manager.aktivni_hrac == 1
+    assert ctrl.selected_square is None
